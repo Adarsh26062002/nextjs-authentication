54c57daeaeff089781a77b41425ca54932de02fb
Adarsh26062002 <nagodadarshsingh@gmail.com>
2024-01-25 12:04:02 +0530

    Middleware added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,34 @@
+'use client'
 import axios from 'axios'
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 
 type Props = {}
 
 const Profile = (props: Props) => {
   const router = useRouter()
+  const [data, setData] = useState('nothing');
   const logout = async()=>{
     try {
-      await axios.get('/api/logout');
+      await axios.get('/api/users/logout');
       toast.success("Logout successful");
       router.push('/login');
     } catch (error) {
-      
+      console.log(error);
     }
+  }
+
+  const getUserDetails = async()=>{
+    const res = await axios.get('/api/users/me');
+    console.log(res.data);
+    setData(res.data.data._id);
   }
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
     <div>Profile</div>
     <hr />
-    <button className=' bg-blue-500 text-white p-4 mt-4 rounded-xl'>Logout</button>
+    <button className=' bg-blue-500 text-white p-4 mt-4 rounded-xl' onClick={logout}>Logout</button>
     </div>
   )
 }
